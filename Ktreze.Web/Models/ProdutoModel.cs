976a1830a803cd579595e06f7696d80715abf83a
Alan Victor Dias Vieira <alan.victor2000@hotmail.com>
2017-10-16 00:12:23 -0200

    Criação da classe ProdutoModel e das Actions de cadastro de produto na HomeController.

---
+++ Ktreze.Web/Models/ProdutoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ktreze.Web.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o código do processo.")]
+        [Display(Name = "Código do processo: ")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o nome do produto.")]
+        [Display(Name = "Nome do produto: ")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o preço de venda.")]
+        [Display(Name = "Preço de venda: ")]
+        public float PrecoVenda { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o preço de compra.")]
+        [Display(Name = "Preço de compra:")] //label
+        public float PrecoCompra { get; set; } //campo
+
+    }
+}
