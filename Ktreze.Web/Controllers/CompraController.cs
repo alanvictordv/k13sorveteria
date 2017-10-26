e1de19456111bece23a5ef8ee7d464fc2ba9a28a
alanvictordv <alanvictordiasvieira@gmail.com>
2017-10-26 18:34:59 -0200

    Criação de página de consulta e métodos para realizar compra.

---
+++ Ktreze.Web/Controllers/CompraController.cs
@@ -0,0 +1,53 @@
+﻿using Ktreze.Web.Models;
+using KTreze.Dados.Entidades;
+using KTreze.Dados.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ktreze.Web.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroCompra(int id)
+        {
+
+            return View();
+        }
+        public ActionResult CadastrarCompra()
+        {
+            return View();
+        }
+
+        public ActionResult ConsultaCompra()
+        {
+            EstoqueDados eDados = new EstoqueDados();
+            ProdutoDados pDados = new ProdutoDados();
+            FreezerDados fDados = new FreezerDados();
+
+            List<CompraModel> listcm = new List<CompraModel>();
+            List<Produto> lista = (List<Produto>)pDados.ListarTodos();
+
+            foreach (Produto p in lista)
+            {
+                CompraModel cm = new CompraModel();
+
+                cm.CodProd = p.Codigo;
+                cm.NomeProd = p.Nome;
+                cm.PrecoCompra = p.PrecoCompra;
+
+                listcm.Add(cm);
+            }
+
+            return View(listcm);
+        }
+    }
+}
---
+++ Ktreze.Web/Models/CompraModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ktreze.Web.Models
+{
+    public class CompraModel
+    {
+        public string CodProd { get; set; }
+        public string NomeProd { get; set; }
+        public decimal? PrecoCompra { get; set; }
+    }
+
+    public class CadastroCompraModel
+    {
+        //TODO: continuar daqui: COMPRAMODEL E COMPRACONTROLLER
+        public string Freezer { get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}
