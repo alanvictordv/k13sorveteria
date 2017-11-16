852712fbfa0417c6bbbcb6caef3330dc2a1419ef
Alan Victor Dias Vieira <alan.victor2000@hotmail.com>
2017-11-16 00:05:57 -0200

    ajustes de design e relatório datatables.

--- Ktreze.Web/App_Start/BundleConfig.cs
+++ Ktreze.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,20 @@ namespace Ktreze.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+               "~/Scripts/datatables/dataTables.min.js",
+               "~/Scripts/datatables/swf/copy_csv_xls_pdf.swf",
+               "~/Scripts/datatables/dataTables.bootstrap.js",
+               "~/Scripts/datatables/dataTables.buttons.min.js",
+               "~/Scripts/datatables/buttons.flash.min.js",
+               "~/Scripts/datatables/buttons.html5.min.js",
+               "~/Scripts/datatables/jquery.dataTables.min.js",
+               "~/Scripts/datatables/jszip.min.js",
+               "~/Scripts/datatables/pdfmake.min.js",
+               "~/Scripts/datatables/vfs_fonts.js"
+               ));
+
         }
     }
 }
